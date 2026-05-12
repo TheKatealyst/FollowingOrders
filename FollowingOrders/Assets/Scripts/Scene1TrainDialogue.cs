d347331cc4d04998316b318212b2ad73d804a3db
Katelyn <donutknife99@gmail.com>
2026-05-12 22:05:03 +0200

    Created visual interest.
    Added font, images, and implemented these. Also started work on the scene script.

--- FollowingOrders/Assets/Scripts/Scene1TrainDialogue.cs
+++ FollowingOrders/Assets/Scripts/Scene1TrainDialogue.cs
@@ -13,7 +13,6 @@ public class Scene1TrainDialogue : MonoBehaviour
     public int primeInt = 1;        // This integer drives game progress!
     public TMP_Text Char1name;
     public TMP_Text Char1speech;
-    public TMP_Text Char2name;
     public TMP_Text Char2speech;
     //public TMP_Text Char3name;
     //public TMP_Text Char3speech;
@@ -70,7 +69,13 @@ public class Scene1TrainDialogue : MonoBehaviour
         primeInt += 1;
         if (primeInt == 1)
         {
-            // audioSource1.Play();
+            ArtChar1a.SetActive(true);
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2speech.text = "You find yourself in the driver's cab of a large train. " +
+                "The radio begins emitting static buzz.";
+            Choice1a.SetActive(true); // function Choice1aFunct()
         }
         else if (primeInt == 2)
         {
@@ -78,14 +83,14 @@ public class Scene1TrainDialogue : MonoBehaviour
             DialogueDisplay.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
+
             Char2speech.text = "Wakey wakey, human.";
         }
         else if (primeInt == 3)
         {
             Char1name.text = "YOU";
             Char1speech.text = "Wuh..? What happened?";
-            Char2name.text = "";
+
             Char2speech.text = "";
             //gameHandler.AddPlayerStat(1);
         }
@@ -93,35 +98,30 @@ public class Scene1TrainDialogue : MonoBehaviour
         {
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "I know I did not hit you that hard.";
         }
         else if (primeInt == 5)
         {
             Char1name.text = "YOU";
             Char1speech.text = "Hit me? Why?";
-            Char2name.text = "";
             Char2speech.text = "";
         }
         else if (primeInt == 6)
         {
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "I am searching for a fugitive. Ragu Fahn.";
         }
         else if (primeInt == 7)
         {
             Char1name.text = "YOU";
             Char1speech.text = "Why do you think I know anything?";
-            Char2name.text = "";
             Char2speech.text = "";
         }
         else if (primeInt == 8)
         {
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "Do not play the stupid. You will take me to him.";
             // Turn off the "Next" button, turn on "Choice" buttons
             nextButton.SetActive(false);
@@ -136,14 +136,12 @@ public class Scene1TrainDialogue : MonoBehaviour
             //gameHandler.AddPlayerStat(1);
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "Then you are no use to me, and must be silenced.";
         }
         else if (primeInt == 21)
         {
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "Come back here! Do not think you can hide from me!";
             // Turn off the "Next" button, turn on "Scene" button/s
             nextButton.SetActive(false);
@@ -156,14 +154,12 @@ public class Scene1TrainDialogue : MonoBehaviour
         {
             Char1name.text = "";
             Char1speech.text = "";
-            Char2name.text = "Jeda";
             Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
         }
         else if (primeInt == 31)
         {
             Char1name.text = "YOU";
             Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
-            Char2name.text = "";
             Char2speech.text = "";
             // Turn off the "Next" button, turn on "Scene" button/s
             nextButton.SetActive(false);
@@ -179,7 +175,6 @@ public class Scene1TrainDialogue : MonoBehaviour
     {
         Char1name.text = "YOU";
         Char1speech.text = "I don't know what you're talking about!";
-        Char2name.text = "";
         Char2speech.text = "";
         primeInt = 19; // so hitting "NEXT" goes to primeInt==20!
         Choice1a.SetActive(false);
@@ -191,7 +186,6 @@ public class Scene1TrainDialogue : MonoBehaviour
     {
         Char1name.text = "YOU";
         Char1speech.text = "Sure, anything you want... just lay off the club.";
-        Char2name.text = "";
         Char2speech.text = "";
         primeInt = 29; // so hitting "NEXT" goes to primeInt==30!
         Choice1a.SetActive(false);
