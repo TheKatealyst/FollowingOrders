bad4ef1713d281d7de00f8d6d51ff0c4ad01e16a
Katelyn <donutknife99@gmail.com>
2026-05-14 22:51:18 +0200

    Finished first scene

--- FollowingOrders/Assets/Scripts/Scene1TrainDialogue.cs
+++ FollowingOrders/Assets/Scripts/Scene1TrainDialogue.cs
@@ -17,7 +17,7 @@ public class Scene1TrainDialogue : MonoBehaviour
     //public TMP_Text Char3name;
     //public TMP_Text Char3speech;
     public GameObject DialogueDisplay;
-    public GameObject ArtChar1a;
+    //public GameObject ArtChar1a;
     //public GameObject ArtChar1b;
     //public GameObject ArtChar1c;
     //public GameObject ArtChar2;
@@ -27,6 +27,7 @@ public class Scene1TrainDialogue : MonoBehaviour
     public GameObject NextScene1Button;
     public GameObject NextScene2Button;
     public GameObject nextButton;
+    public GameObject CharBox;
     //public AudioSource audioSource1;
     private bool allowSpace = true;
 
@@ -34,7 +35,6 @@ public class Scene1TrainDialogue : MonoBehaviour
     void Start()
     {
         DialogueDisplay.SetActive(false);
-        ArtChar1a.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
@@ -69,98 +69,54 @@ public class Scene1TrainDialogue : MonoBehaviour
         primeInt += 1;
         if (primeInt == 1)
         {
-            ArtChar1a.SetActive(true);
+            CharBox.SetActive(false);
+            nextButton.SetActive(false);
+            allowSpace = false;
             DialogueDisplay.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
-            Char2speech.text = "You find yourself in the driver's cab of a large train. " +
+            Char2speech.text = "You find yourself in the driver's cab of a large train." +
                 "The radio begins emitting static buzz.";
             Choice1a.SetActive(true); // function Choice1aFunct()
         }
         else if (primeInt == 2)
         {
-            ArtChar1a.SetActive(true);
+            Choice1a.SetActive(false);
+            CharBox.SetActive(true);
+            nextButton.SetActive(true);
+            allowSpace = true;
             DialogueDisplay.SetActive(true);
-            Char1name.text = "";
-            Char1speech.text = "";
-
-            Char2speech.text = "Wakey wakey, human.";
+            Char2speech.text = "";
+            Char1name.text = "Commanding Officer";
+            Char1speech.text = "//Krztzzz..// \"Hi there friend! It's good to see you at your post. We have high hopes for you!\"";
         }
         else if (primeInt == 3)
         {
-            Char1name.text = "YOU";
-            Char1speech.text = "Wuh..? What happened?";
-
+            nextButton.SetActive(false);
+            allowSpace = false;
+            DialogueDisplay.SetActive(true);
             Char2speech.text = "";
-            //gameHandler.AddPlayerStat(1);
+            Char1name.text = "Commanding Officer";
+            Char1speech.text = "\"Now your job is quite simple: Set the train in motion, ensure you arrive at the designated locations, manage your cargo, and deliver it at the requested drop-off points.\"";
+            Choice1b.SetActive(true);
         }
         else if (primeInt == 4)
         {
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "I know I did not hit you that hard.";
+            Choice1b.SetActive(false);
+            nextButton.SetActive(true);
+            allowSpace = true;
+            DialogueDisplay.SetActive(true);
+            Char2speech.text = "";
+            Char1name.text = "Commanding Officer";
+            Char1speech.text = "Your commanding officer continues:\r\n\r\n\"Now, your important duties will be as follows:\"\r\n''\r\n- Perform pre-departure train inspections before leaving each station.\r\n- Select the designated travel location.\r\n- Let our people load in the cargo.\r\n- Manage the cargo.\r\n- Inform Headquarters of any irregularities.\r\n''\r\n";
         }
+
         else if (primeInt == 5)
         {
-            Char1name.text = "YOU";
-            Char1speech.text = "Hit me? Why?";
+            DialogueDisplay.SetActive(true);
             Char2speech.text = "";
-        }
-        else if (primeInt == 6)
-        {
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "I am searching for a fugitive. Ragu Fahn.";
-        }
-        else if (primeInt == 7)
-        {
-            Char1name.text = "YOU";
-            Char1speech.text = "Why do you think I know anything?";
-            Char2speech.text = "";
-        }
-        else if (primeInt == 8)
-        {
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "Do not play the stupid. You will take me to him.";
-            // Turn off the "Next" button, turn on "Choice" buttons
-            nextButton.SetActive(false);
-            allowSpace = false;
-            Choice1a.SetActive(true); // function Choice1aFunct()
-            Choice1b.SetActive(true); // function Choice1bFunct()
-        }
-
-        // after choice 1a
-        else if (primeInt == 20)
-        {
-            //gameHandler.AddPlayerStat(1);
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "Then you are no use to me, and must be silenced.";
-        }
-        else if (primeInt == 21)
-        {
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "Come back here! Do not think you can hide from me!";
-            // Turn off the "Next" button, turn on "Scene" button/s
-            nextButton.SetActive(false);
-            allowSpace = false;
-            NextScene1Button.SetActive(true);
-        }
-
-        // after choice 1b
-        else if (primeInt == 30)
-        {
-            Char1name.text = "";
-            Char1speech.text = "";
-            Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
-        }
-        else if (primeInt == 31)
-        {
-            Char1name.text = "YOU";
-            Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
-            Char2speech.text = "";
+            Char1name.text = "Commanding Officer";
+            Char1speech.text = "\"These tasks may appear in a different order and I understand it can be quite overwhelming so let's run through them together shall we friend?\" //..KKzzt//";
             // Turn off the "Next" button, turn on "Scene" button/s
             nextButton.SetActive(false);
             allowSpace = false;
@@ -173,30 +129,14 @@ public class Scene1TrainDialogue : MonoBehaviour
     // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
     public void Choice1aFunct()
     {
-        Char1name.text = "YOU";
-        Char1speech.text = "I don't know what you're talking about!";
-        Char2speech.text = "";
-        primeInt = 19; // so hitting "NEXT" goes to primeInt==20!
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
-        nextButton.SetActive(true);
-        allowSpace = true;
     }
     public void Choice1bFunct()
     {
-        Char1name.text = "YOU";
-        Char1speech.text = "Sure, anything you want... just lay off the club.";
-        Char2speech.text = "";
-        primeInt = 29; // so hitting "NEXT" goes to primeInt==30!
-        Choice1a.SetActive(false);
-        Choice1b.SetActive(false);
-        nextButton.SetActive(true);
-        allowSpace = true;
     }
 
     public void SceneChange1()
     {
-        SceneManager.LoadScene("Scene2a");
+
     }
     public void SceneChange2()
     {
