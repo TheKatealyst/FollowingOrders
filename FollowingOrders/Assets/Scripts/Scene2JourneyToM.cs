6ed202ec742323de17b89973f505c5998caf898f
Katelyn <donutknife99@gmail.com>
2026-05-20 12:21:29 +0200

    Fnished Scene 2 Journey.

---
+++ FollowingOrders/Assets/Scripts/Scene2JourneyToM.cs
@@ -0,0 +1,135 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene2JourneyToM : MonoBehaviour
+{
+    // These are the script variables.
+    // For more character images / buttons, copy & renumber the variables:
+    public int primeInt = 1;        // This integer drives game progress!
+    public TMP_Text Char1name;
+    public TMP_Text Char1speech;
+    public GameObject ScrollViewChar1;
+    //public TMP_Text Char3name;
+    //public TMP_Text Char3speech;
+    public GameObject DialogueDisplay;
+    //public GameObject ArtChar1a;
+    //public GameObject ArtChar1b;
+    //public GameObject ArtChar1c;
+    //public GameObject ArtChar2;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    public GameObject CharBox;
+    //public AudioSource audioSource1;
+    private bool allowSpace = true;
+
+    // Set initial visibility. Added images or buttons need to also be SetActive(false);
+    void Start()
+    {
+        DialogueDisplay.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    // Use the spacebar as a faster "Next" button:
+    void Update()
+    {
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Next();
+            }
+
+            // secret debug code: go back 1 Story Unit, if NEXT is visible
+            if (Input.GetKeyDown("p"))
+            {
+                primeInt -= 2;
+                Next();
+            }
+        }
+    }
+
+    //Story Units! The main story function.
+    //Players hit [NEXT] to progress to the next primeInt:
+    public void Next()
+    {
+        primeInt += 1;
+        if (primeInt == 1)
+        {
+            CharBox.SetActive(false);
+            nextButton.SetActive(true);
+            allowSpace = true;
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "The train loudly comes alive as the electricity runs through the mighty machine.";
+        }
+        else if (primeInt == 2)
+        {
+            Choice1a.SetActive(false);
+            CharBox.SetActive(false);
+            nextButton.SetActive(true);
+            allowSpace = true;
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "The train consists of a main locomotive and 25 carriages which, though empty at the moment, will be filled with cargo momentarily at the central station.";
+        }
+        else if (primeInt == 3)
+        {
+            nextButton.SetActive(true);
+            allowSpace = true;
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "The flat green plains and farms of the country pass by your train.";
+        }
+        else if (primeInt == 4)
+        {
+            nextButton.SetActive(true);
+            allowSpace = true;
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "The sun shines brightly upon the small towns and cities in the distance.\r\n";
+        }
+
+        else if (primeInt == 5)
+        {
+            DialogueDisplay.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "The train trip is short, within a brisk 15 minutes the skyline of central city comes into view.";            
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene2Button.SetActive(true);
+        }
+
+        //Please do NOT delete this final bracket that ends the Next() function:
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+    public void Choice1aFunct()
+    {
+    }
+    public void Choice1bFunct()
+    {
+    }
+
+    public void SceneChange1()
+    {
+
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Scene2b");
+    }
+}
