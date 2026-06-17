3088730f378474caedc91c7edd73c7aa6fc2bf8d
Katelyn <donutknife99@gmail.com>
2026-06-17 23:25:51 +0200

    Created audio manager

--- FollowingOrders/Assets/Scripts/AudioManagerScript.cs
+++ FollowingOrders/Assets/Scripts/AudioManagerScript.cs
@@ -4,58 +4,23 @@ using UnityEngine;
 
 public class AudioManagerScript : MonoBehaviour
 {
-	// Audio players components.
-	public AudioSource EffectsSource;
-	public AudioSource MusicSource;
+    public AudioSource SFXClip1;
+    public AudioSource SFXClip2;
+    public AudioSource SFXClip3;
 
-	// Random pitch adjustment range.
-	public float LowPitchRange = .95f;
-	public float HighPitchRange = 1.05f;
-
-	// Singleton instance.
-	public static AudioManagerScript Instance = null;
-	
-	// Initialize the singleton instance.
-	private void Awake()
-	{
-		// If there is not already an instance of SoundManager, set it to this.
-		if (Instance == null)
-		{
-			Instance = this;
-		}
-		//If an instance already exists, destroy whatever this object is to enforce the singleton.
-		else if (Instance != this)
-		{
-			Destroy(gameObject);
-		}
-
-		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-		DontDestroyOnLoad (gameObject);
-	}
-
-	// Play a single clip through the sound effects source.
-	public void Play(AudioClip clip)
-	{
-		EffectsSource.clip = clip;
-		EffectsSource.Play();
-	}
-
-	// Play a single clip through the music source.
-	public void PlayMusic(AudioClip clip)
-	{
-		MusicSource.clip = clip;
-		MusicSource.Play();
-	}
-
-	// Play a random clip from an array, and randomize the pitch slightly.
-	public void RandomSoundEffect(params AudioClip[] clips)
-	{
-		int randomIndex = Random.Range(0, clips.Length);
-		float randomPitch = Random.Range(LowPitchRange, HighPitchRange);
-
-		EffectsSource.pitch = randomPitch;
-		EffectsSource.clip = clips[randomIndex];
-		EffectsSource.Play();
-	}
-	
+    void Update()
+    {
+        if (Input.GetKeyDown("5"))
+        {
+            SFXClip1.Play();
+        }
+        if (Input.GetKeyDown("6"))
+        {
+            SFXClip2.Play();
+        }
+        if (Input.GetKeyDown("7"))
+        {
+            SFXClip2.Play();
+        }
+    }
 }
